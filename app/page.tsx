2326036ff924224c9a9928d03ba4f1e89929632c
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-10-09 00:30:39 -0700

    Update: add nav links & onboarding component

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,9 @@ export default function Home() {
   return (
     <>
       <SignedOut>
-        <div className="container h-screen w-full flex flex-col justify-center items-center max-w-4xl">
-          <p className="w-full text-lg md:text-center tracking-wide font-light uppercase mb-2">
-            Make shopping feel like magic again!
-          </p>
-          <h1 className="text-5xl md:text-center font-extrabold">
-            Transform your shopping experience with the{" "}
-            <span className="text-blue-800">Greatest</span> tracking solution.
-          </h1>
-          <div className="mt-6 flex flex-col md:flex-row justify-center items-center gap-2 w-full">
-            <Button as="a" size="lg" href="/sign-up" color="dark">
-              Get Started
-            </Button>
-            <Button as="a" size="lg" href="/sign-in">
-              Sign In
-            </Button>
-          </div>
-        </div>
+        <OnBoarding />
       </SignedOut>
-
+      
       <SignedIn>
         <div className="container h-screen w-full flex flex-col justify-center items-center max-w-4xl">
           <h1 className="text-5xl md:text-center font-extrabold">
@@ -38,4 +22,26 @@ export default function Home() {
       </SignedIn>
     </>
   );
+}
+
+export function OnBoarding() {
+  return (
+    <div className="container h-screen w-full flex flex-col justify-center items-center max-w-4xl">
+      <p className="w-full text-lg md:text-center tracking-wide font-light uppercase mb-2">
+        Make shopping feel like magic again!
+      </p>
+      <h1 className="text-5xl md:text-center font-extrabold">
+        Transform your shopping experience with the{" "}
+        <span className="text-blue-800">Greatest</span> tracking solution.
+      </h1>
+      <div className="mt-6 flex flex-col md:flex-row justify-center items-center gap-2 w-full">
+        <Button as="a" size="lg" href="/sign-up" color="dark">
+          Get Started
+        </Button>
+        <Button as="a" size="lg" href="/sign-in">
+          Sign In
+        </Button>
+      </div>
+    </div>
+  )
 }
